d707e935a4f88561ecbec27c5e8eaa26a7fff16a
7995283828 <41770630+7995283828@users.noreply.github.com>
2018-08-26 19:29:53 +0530

    Update leap.py

--- leap.py
+++ leap.py
@@ -1,7 +1,7 @@
 year = int(input("Please Enter the Year Number you wish: "))
  
 if (( year%400 == 0)or (( year%4 == 0 ) and ( year%100 != 0))):
-    print("%d is a Leap Year" %year)
+    print("%d is yes" %year)
 else:
-    print("%d is Not the Leap Year" %year)
+    print("%d is no" %year)
     
