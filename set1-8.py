61ec8e3b36c831ff3cc17d55b2c4a90faec4baae
7995283828 <41770630+7995283828@users.noreply.github.com>
2018-09-09 10:33:00 +0530

    Update set1-8.py

--- set1-8.py
+++ set1-8.py
@@ -1,6 +1,6 @@
-sum=int(input("Enter a number: "))
+sam=int(input("Enter a number: "))
 sum1 = 0
-while(sum > 0):
-    sum1=sum1+sum
+while(sam > 0):
+    sum1=sum1+sam
     sum=sum-1
 print("The sum of first n natural numbers is",sum1)
